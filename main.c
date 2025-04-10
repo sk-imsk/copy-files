a2bcb20bcad42cefb2fab6bc6d9201f2d12e08f0
sk-imsk <pikachududee@gmail.com>
2025-04-10 15:31:18 +1000

    Update main.c
    made the program create a file as a start

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 // made by sk
 
 
-#import <stdio.h>
+#include <stdio.h>
+#include <string.h>
 
 
 int main(int argc, char * argv[]){
-  printf("This program is still in development;
-  return 0;
+  if (argc == 1){
+      printf("usage: copy filename");
+    return 1;
 }
+    struct filestuff {
+      char filename[20];
+      char filedata[100];
+    };
+    struct filestuff file;
+    strncpy(file.filename, argv[1], sizeof(file.filename) - 1);
+    file.filename[sizeof(file.filename) - 1] = '\0';
+    
+    FILE *filepointer;
+    filepointer = fopen(file.filename, "w");
+  
+    fclose(filepointer);
+}
+
