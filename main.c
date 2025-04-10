30c552a0ea30cf86dd1263cd589ae1976319d3d9
sk-imsk <pikachududee@gmail.com>
2025-04-10 19:50:24 +1000

    Update main.c
    added feature to print whole file (im too lazy to make it copy to the file instead of stdout)

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 
 int main(int argc, char * argv[]){
+  printf("this is in the early stages of development please report bugs to https://github.com/sk-imsk/copy-files/issues\n")
   if (argc == 1){
       printf("usage: copy filename");
     return 1;
@@ -17,10 +19,17 @@ int main(int argc, char * argv[]){
     struct filestuff file;
     strncpy(file.filename, argv[1], sizeof(file.filename) - 1);
     file.filename[sizeof(file.filename) - 1] = '\0';
-    
+    // error check
     FILE *filepointer;
-    filepointer = fopen(file.filename, "w");
+    filepointer = fopen(file.filename, "r");
+    if (filepointer == NULL){
+      perror("error");
+      return 2;
+      }
+    // start reading and writeing file
+    while(fgets(file.filedata, 99, filepointer)){
+        printf("%s", filedata);}
   
-    fclose(filepointer);
+  fclose(filepointer);
 }
 
